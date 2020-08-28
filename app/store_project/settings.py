cc7b7862b6ea7ef3e196f0d5a087283701ed28b0
lancegoyke <lancegoyke@gmail.com>
2020-08-28 07:22:46 -0700

    add upload app - media uploads in dev

--- app/store_project/settings.py
+++ app/store_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "upload",
 ]
 
 MIDDLEWARE = [
@@ -124,3 +125,5 @@ USE_TZ = True
 
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_URL = "/mediafiles/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
