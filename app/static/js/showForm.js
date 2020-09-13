5ef9355a6d06f37614a366209f792bcbc464d25a
lancegoyke <lancegoyke@gmail.com>
2020-09-13 08:37:26 -0700

    signup page; move email toggle to showForm.js

---
+++ app/static/js/showForm.js
@@ -0,0 +1,8 @@
+function showForm() {
+  let x = document.getElementById("email-form");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
