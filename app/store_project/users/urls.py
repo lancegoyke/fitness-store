6b3142d9e379fd616900ba102647bc9af389ee72
lancegoyke <lancegoyke@gmail.com>
2020-10-27 10:35:44 -0700

    remove UserDetailView

--- app/store_project/users/urls.py
+++ app/store_project/users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 from store_project.users.views import (
     user_profile_view,
-    user_detail_view,
     user_redirect_view,
     user_update_view,
 )
@@ -12,5 +11,4 @@ urlpatterns = [
     path("profile/", view=user_profile_view, name="profile"),
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
-    path("<str:username>/", view=user_detail_view, name="detail"),
 ]
--- app/store_project/users/views.py
+++ app/store_project/users/views.py
@@ -18,20 +18,10 @@ class UserProfileView(LoginRequiredMixin, DetailView):
     }
 
     def get_object(self):
-        return User.objects.get(username=self.request.user.username)
+        return User.objects.get(email=self.request.user.email)
 
 
 user_profile_view = UserProfileView.as_view()
-
-
-class UserDetailView(LoginRequiredMixin, DetailView):
-
-    model = User
-    slug_field = "username"
-    slug_url_kwarg = "username"
-
-
-user_detail_view = UserDetailView.as_view()
 
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
@@ -40,14 +30,14 @@ class UserUpdateView(LoginRequiredMixin, UpdateView):
     fields = ["name"]
 
     def get_success_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("users:detail", kwargs={"email": self.request.user.email})
 
     def get_object(self):
-        return User.objects.get(username=self.request.user.username)
+        return User.objects.get(email=self.request.user.email)
 
     def form_valid(self, form):
         messages.add_message(
-            self.request, messages.INFO, _("Infos successfully updated")
+            self.request, messages.INFO, _("Info successfully updated")
         )
         return super().form_valid(form)
 
@@ -60,7 +50,7 @@ class UserRedirectView(LoginRequiredMixin, RedirectView):
     permanent = False
 
     def get_redirect_url(self):
-        return reverse("users:detail", kwargs={"username": self.request.user.username})
+        return reverse("users:detail", kwargs={"email": self.request.user.email})
 
 
 user_redirect_view = UserRedirectView.as_view()
