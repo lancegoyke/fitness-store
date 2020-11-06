61fcdcf73d4213f1220781318a3a7c882dd67b83
lancegoyke <lancegoyke@gmail.com>
2020-11-06 10:22:25 -0800

    products factories.py

---
+++ app/store_project/products/factories.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+import random
+
+from django.utils.text import slugify
+
+import factory
+from factory.django import DjangoModelFactory
+
+from store_project.users.factories import UserFactory, SuperAdminFactory
+from .models import Product, Program
+
+
+class ProgramFactory(DjangoModelFactory):
+    class Meta:
+        model = Program
+
+    name = factory.Faker("sentence", nb_words=5, variable_nb_words=True)
+    description = factory.Faker("paragraph", nb_sentences=2, variable_nb_sentences=True)
+    slug = factory.LazyAttribute(lambda o: slugify(o.name))
+    status = Product.PUBLIC
+    price = 10.00
+    author = factory.SubFactory(SuperAdminFactory)
+    page_content = factory.Faker(
+        "paragraph", nb_sentences=5, variable_nb_sentences=True
+    )
+    duration = factory.LazyFunction(lambda: random.randint(2, 8))
+    frequency = factory.LazyFunction(lambda: random.randint(3, 7))
