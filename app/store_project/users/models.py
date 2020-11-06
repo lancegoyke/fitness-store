60edd7dc61ebfa6267f0fad69902ba16ace1a0e7
lancegoyke <lancegoyke@gmail.com>
2020-11-06 10:17:38 -0800

    trim user model, add stripe_customer_id

--- app/store_project/users/models.py
+++ app/store_project/users/models.py
@@ -8,19 +8,12 @@ class User(AbstractUser):
     """Default user for store_project."""
 
     #: First and last name do not cover name patterns around the globe
-    username = None
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    email = EmailField(_("Email Address"), unique=True)
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
+    stripe_customer_id = CharField(_("Stripe Customer ID"), max_length=100, blank=True)
 
     def __str__(self):
         return self.email
 
     def get_absolute_url(self):
-        """Get url for user's detail view.
-
-        Returns:
-            str: URL for user detail.
-        """
-        return reverse("users:detail", kwargs={"username": self.username})
+        """Get url for user's profile view."""
+        return reverse("users:profile")
