f889f609757fb647e5ba9030321790dd3b9ac03c
lancegoyke <lancegoyke@gmail.com>
2020-11-09 10:11:27 -0800

    add pytest fixture for ProgramFactory()

--- app/store_project/conftest.py
+++ app/store_project/conftest.py
@@ -2,6 +2,8 @@ import pytest
 
 from store_project.users.models import User
 from store_project.users.factories import UserFactory
+from store_project.products.models import Program
+from store_project.products.factories import ProgramFactory
 
 
 @pytest.fixture(autouse=True)
@@ -12,3 +14,8 @@ def media_storage(settings, tmpdir):
 @pytest.fixture
 def user() -> User:
     return UserFactory()
+
+
+@pytest.fixture
+def program() -> Program:
+    return ProgramFactory()
