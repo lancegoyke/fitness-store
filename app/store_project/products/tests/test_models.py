2a234baf487b7e4a4a5d36d80947095b43c3a2ef
lancegoyke <lancegoyke@gmail.com>
2020-11-10 11:31:33 -0800

    add products test_models.py

---
+++ app/store_project/products/tests/test_models.py
@@ -0,0 +1,37 @@
+from decimal import Decimal
+
+from django.contrib.auth.models import Group, Permission
+
+import pytest
+
+from store_project.products.factories import ProgramFactory
+from store_project.products.models import Product, Program
+
+pytestmark = pytest.mark.django_db
+
+
+def test_program_get_absolute_url(program: Program):
+    assert program.get_absolute_url() == f"/programs/{program.slug}/"
+
+
+def test_program_is_public(program: Program):
+    public_program = program
+    assert public_program.is_public()
+    draft_program = ProgramFactory(status=Product.DRAFT)
+    assert not draft_program.is_public()
+    private_program = ProgramFactory(status=Product.PRIVATE)
+    assert not private_program.is_public()
+
+
+def test_program_add_permission(program: Program):
+    """Test that lifecycle hook has created a Permission for this program"""
+    assert Permission.objects.get(codename=f"can_view_{program.slug}")
+    assert Permission.objects.get(name=f"Can view {program.name}")
+
+
+def test_program_remove_permission(program: Program):
+    """Bypassing the lifecycle hook due to errors with Stripe."""
+    program.remove_program_permission()
+    with pytest.raises(Permission.DoesNotExist):
+        assert Permission.objects.get(codename=f"can_view_{program.slug}")
+        assert Permission.objects.get(name=f"Can view {program.name}")
