4dc2cfc633fe51d8b58524d7123c9bc203c39a1b
lancegoyke <lancegoyke@gmail.com>
2020-11-11 12:54:30 -0800

    create payments test_views.py

--- app/store_project/payments/tests/test_views.py
+++ app/store_project/payments/tests/test_views.py
@@ -2,6 +2,7 @@ import pytest
 
 from django.contrib.auth.models import AnonymousUser
 from django.contrib.messages import get_messages
+from django.http import HttpResponseRedirect, JsonResponse
 from django.test import Client, RequestFactory
 
 from store_project.payments import views
@@ -13,14 +14,41 @@ from store_project.users.models import User
 pytestmark = pytest.mark.django_db
 
 
-def test_login_before_purchase_anonymous_view(program: Program, rf: RequestFactory):
-    request = rf.get(f"/payments/login-to-purchase/{program.slug}/")
-    request.user = AnonymousUser()
-    request._messages = []
-
+def test_login_before_purchase_view(program: Program):
     response = Client().get(f"/payments/login-to-purchase/{program.slug}/")
     messages = list(get_messages(response.wsgi_request))
 
-    assert response.status_code == 302
     assert len(messages) == 1
     assert str(messages[0]) == "You must be logged in to purchase."
+    assert response.status_code == 302
+    assert response.url == f"/accounts/login/?next=/programs/{program.slug}/"
+    assert isinstance(response, HttpResponseRedirect)
+
+
+def test_stripe_config_view():
+    response = Client().get("/payments/config/")
+    assert response.status_code == 200
+    assert isinstance(response, JsonResponse)
+    assert b"publicKey" in response.content
+
+
+def test_create_checkout_session_view_error(program: Program):
+    response = Client().get(
+        f"/payments/create-checkout-session/?program-slug={program.slug}"
+    )
+    assert response.status_code == 200
+    assert isinstance(response, JsonResponse)
+    assert b"error" in response.content
+
+
+def test_create_checkout_session_view(user: User, program: Program, rf: RequestFactory):
+    request = rf.get(f"/payments/create-checkout-session/?program-slug={program.slug}")
+    request.user = user
+    response = views.create_checkout_session(request)
+    assert response.status_code == 200
+    assert isinstance(response, JsonResponse)
+    assert b"sessionId" in response.content
+
+
+def test_stripe_webhook_view():
+    request = rf.get(f"/payments/webhook/")
