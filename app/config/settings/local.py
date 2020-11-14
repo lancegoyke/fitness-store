36787e21b133d30f6e093ccf446de2b745057e1c
lancegoyke <lancegoyke@gmail.com>
2020-11-14 11:56:10 -0800

    add settings for filebased email backend

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -24,4 +24,6 @@ INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
 TEST_RUNNER = "django.test.runner.DiscoverRunner"
 
 # Email
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = APP_DIR / "tmp" / "dev-emails"
