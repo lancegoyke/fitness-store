8671fd944d68ab1436a862633652982ccf27d0b8
lancegoyke <lancegoyke@gmail.com>
2020-11-21 12:06:12 -0800

    manage.py project path and DJANGO_SETTINGS_MODULE

--- app/manage.py
+++ app/manage.py
@@ -2,11 +2,12 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from pathlib import Path
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_project.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,8 +16,14 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+
+    # This allows easy placement of apps within the interior
+    # store_project/ directory.
+    current_path = Path(__file__).parent.resolve()
+    sys.path.append(str(current_path / "store_project"))
+
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
