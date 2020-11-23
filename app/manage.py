16105bcd2984f63a3d98c6893da7dbef6cdb872d
lancegoyke <lancegoyke@gmail.com>
2020-11-22 17:46:13 -0800

    typo

--- app/manage.py
+++ app/manage.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base.")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
