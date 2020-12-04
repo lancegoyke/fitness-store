d59bd394d370dd34b2e03aaf0f5d9d2e403a562c
lancegoyke <lancegoyke@gmail.com>
2020-12-04 11:24:29 -0800

    add exercises to sitemap

---
+++ app/store_project/exercises/sitemaps.py
@@ -0,0 +1,14 @@
+from django.contrib.sitemaps import Sitemap
+
+from store_project.exercises.models import Exercise
+
+
+class ExerciseSitemap(Sitemap):
+    priority = 0.5
+    changefreq = "weekly"
+
+    def items(self):
+        return Exercise.objects.all()
+
+    def lastmod(self, obj):
+        return obj.modified
