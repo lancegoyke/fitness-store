15a8765aa6a7bb20f9e25bc1cce2a6f45b84b419
lancegoyke <lancegoyke@gmail.com>
2020-12-18 11:39:04 -0800

    filter exercises by category for staff user

--- app/store_project/exercises/urls.py
+++ app/store_project/exercises/urls.py
@@ -1,9 +1,18 @@
 from django.urls import path
 
-from store_project.exercises.views import ExerciseDetailView, ExerciseListView
+from store_project.exercises.views import (
+    ExerciseDetailView,
+    ExerciseFilteredListView,
+    ExerciseListView
+)
 
 app_name = "exercises"
 urlpatterns = [
     path("", ExerciseListView.as_view(), name="list"),
+    path(
+        "category/<str:category>/",
+        ExerciseFilteredListView.as_view(),
+        name="filtered_list"
+    ),
     path("<str:slug>/", ExerciseDetailView.as_view(), name="detail"),
 ]
--- app/store_project/exercises/views.py
+++ app/store_project/exercises/views.py
@@ -1,6 +1,7 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
-from store_project.exercises.models import Alternative, Exercise
+from store_project.exercises.models import Alternative, Category, Exercise
 
 
 class ExerciseDetailView(DetailView):
@@ -16,3 +17,28 @@ class ExerciseListView(ListView):
     model = Exercise
     context_object_name = "exercises"
     ordering = "name"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["categories"] = Category.objects.all()
+        return context
+    
+
+
+class ExerciseFilteredListView(ListView):
+    model = Exercise
+    context_object_name = "exercises"
+    ordering = "name"
+    template_name = "exercises/exercise_filtered_list.html"
+
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, slug=self.kwargs["category"])
+        return Exercise.objects.filter(categories=self.category)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        self.category = get_object_or_404(Category, slug=self.kwargs["category"])
+        context["category"] = self.category.name
+        return context
+    
+    
