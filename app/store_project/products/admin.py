d6f6495438f530c67ebb49b5f0ad4c0f9dc31569
lancegoyke <lancegoyke@gmail.com>
2021-01-26 14:25:44 -0800

    add books to admin

--- app/store_project/products/admin.py
+++ app/store_project/products/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin, messages
 from django.utils.translation import ngettext
 
-from store_project.products.models import Category, Program
+from store_project.products.models import Category, Program, Book
 
 
 @admin.register(Program)
@@ -63,6 +63,67 @@ class ProgramAdmin(admin.ModelAdmin):
     make_public.short_description = "Mark selected programs as public"
     make_draft.short_description = "Mark selected programs as draft"
     make_private.short_description = "Mark selected programs as private"
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "created",
+        "views",
+        "status",
+    ]
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = [
+        "-created",
+    ]
+    actions = [
+        # Disabled because bulk updates don't trigger django-lifecycle
+        # AFTER_SAVE marketing emails.
+        # "make_public",
+        "make_draft",
+        "make_private",
+    ]
+
+    def make_public(self, request, queryset):
+        updated = queryset.update(status=Book.PUBLIC)
+        self.message_user(
+            request,
+            ngettext(
+                f"{updated} book was successfully marked as public.",
+                f"{updated} books were successfully marked as public.",
+                updated,
+            ),
+            messages.SUCCESS,
+        )
+
+    def make_draft(self, request, queryset):
+        updated = queryset.update(status=Book.DRAFT)
+        self.message_user(
+            request,
+            ngettext(
+                f"{updated} book was successfully marked as draft.",
+                f"{updated} books were successfully marked as draft.",
+                updated,
+            ),
+            messages.SUCCESS,
+        )
+
+    def make_private(self, request, queryset):
+        updated = queryset.update(status=Book.PRIVATE)
+        self.message_user(
+            request,
+            ngettext(
+                f"{updated} book was successfully marked as private.",
+                f"{updated} books were successfully marked as private.",
+                updated,
+            ),
+            messages.SUCCESS,
+        )
+
+    make_public.short_description = "Mark selected books as public"
+    make_draft.short_description = "Mark selected books as draft"
+    make_private.short_description = "Mark selected books as private"
 
 
 @admin.register(Category)
