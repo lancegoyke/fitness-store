9d6e179e83afffd841a35b755b48621f7f886e02
lancegoyke <lancegoyke@gmail.com>
2021-01-29 08:46:16 -0800

    fix merged changes

--- app/store_project/payments/views.py
+++ app/store_project/payments/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.urls import reverse
