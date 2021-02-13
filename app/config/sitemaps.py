38d7bfd3343c4a1851f876950d2f5c8e564f116e
lancegoyke <lancegoyke@gmail.com>
2021-02-13 13:17:05 -0800

    flake8 E231

--- app/config/sitemaps.py
+++ app/config/sitemaps.py
@@ -7,7 +7,7 @@ class StaticViewSitemap(sitemaps.Sitemap):
     changefreq = "daily"
 
     def items(self):
-        return ["products:store", "products:program_list", "products:book_list",]
+        return ["products:store", "products:program_list", "products:book_list", ]
 
     def location(self, item):
         return reverse(item)
