131fb7f627dc9cb674f0ed1167647b77d01d0de1
lancegoyke <lancegoyke@gmail.com>
2021-02-13 12:59:50 -0800

    add BookSitemap

--- app/store_project/products/sitemaps.py
+++ app/store_project/products/sitemaps.py
@@ -1,6 +1,17 @@
 from django.contrib.sitemaps import Sitemap
 
-from store_project.products.models import Program
+from store_project.products.models import Book, Program
+
+
+class BookSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return Book.objects.filter(status=Book.PUBLIC)
+
+    def lastmod(self, obj):
+        return obj.modified
 
 
 class ProgramSitemap(Sitemap):
