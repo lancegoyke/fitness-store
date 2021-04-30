d5a29e33dabe162ad72a12d35fad816008f59d9a
lancegoyke <lancegoyke@gmail.com>
2021-04-29 17:30:34 -0700

    import error

--- app/store_project/notifications/apps.py
+++ app/store_project/notifications/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NotificationsConfig(AppConfig):
-    name = 'notifications'
+    name = "store_project.notifications"
+    verbose_name = _("Notifications")
