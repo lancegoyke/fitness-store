547f76a4d28cb685cb5e9751003a04a09c07e336
lancegoyke <lancegoyke@gmail.com>
2021-05-16 16:59:25 -0700

    disable page caching for the pages that use S3

--- app/store_project/products/urls.py
+++ app/store_project/products/urls.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from .views import (
     BookDetailView,
@@ -15,27 +13,27 @@ app_name = "products"
 urlpatterns = [
     path(
         "store/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(StoreView.as_view()),
+        StoreView.as_view(),
         name="store"
     ),
     path(
         "books/<str:slug>/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(BookDetailView.as_view()),
+        BookDetailView.as_view(),
         name="book_detail"
     ),
     path(
         "books/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(BookListView.as_view()),
+        BookListView.as_view(),
         name="book_list"
     ),
     path(
         "programs/<str:slug>/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(ProgramDetailView.as_view()),
+        ProgramDetailView.as_view(),
         name="program_detail"
     ),
     path(
         "programs/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(ProgramListView.as_view()),
+        ProgramListView.as_view(),
         name="program_list"
     ),
 ]
