9709cc8d48cff8cc4d85546a6daa1d763b0f7661
Adam Johnson <me@adamj.eu>
2023-02-15 15:51:53 +0000

    Remove showForm.js

--- app/static/js/showForm.js
+++
@@ -1,8 +0,0 @@
-function showForm() {
-  let x = document.getElementById("email-form");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
