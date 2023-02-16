ec2d8f0e0dbfb90bd8c0a93ec9ac5e04b69711c1
Lance Goyke <lance@lancegoyke.com>
2023-02-16 12:43:07 -0700

    remove or rename old code

--- app/store_project/tracking/admin.py
+++ app/store_project/tracking/admin.py
@@ -1,15 +1,9 @@
 from django.contrib import admin
 
-from store_project.tracking.models import Category, Test
+from store_project.tracking.models import Category
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ("name", "slug")
-    prepopulated_fields = {"slug": ("name",)}
-
-
-@admin.register(Test)
-class TestAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "created")
     prepopulated_fields = {"slug": ("name",)}
