63a9e974cd51282a619978ce5451f50ea4f332af
Adam Johnson <me@adamj.eu>
2023-02-20 18:51:38 +0000

    Remove TEST_RUNNER setting (#2)

--- app/config/settings/test.py
+++ app/config/settings/test.py
@@ -4,9 +4,6 @@ With these settings, tests run faster.
 
 from .base import *  # noqa
 
-# https://docs.djangoproject.com/en/dev/ref/settings/#test-runner
-TEST_RUNNER = "django.test.runner.DiscoverRunner"
-
 # CACHES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#caches
