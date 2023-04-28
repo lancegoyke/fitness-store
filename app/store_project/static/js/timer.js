167dfa9895f55b6c55c4e3550c6eb8635e42f8f9
Lance Goyke <lancegoyke@gmail.com>
2023-04-28 07:18:21 -0700

    Interval timer (#51)
    * fix timer overlay and reset button
    
    * refactor: add getMinutes() and getSeconds()
    
    * reset and pause

--- app/store_project/static/js/timer.js
+++ app/store_project/static/js/timer.js
@@ -3,15 +3,13 @@
  * Author: Lance Goyke
  *
  * TODO
+ * - [x] Reset button stops the timer
+ * - [x] Pause and resume
+ * - [x] Count in period
  * - [ ] Add audio cues
  */
 
 // DOM Elements
-let timer;
-let elapsedSeconds;
-let totalRoundSeconds;
-let currentRound;
-let isResting;
 const countdown = document.querySelector("#countdown");
 const countdownMinutes = document.querySelector("#minutes");
 const countdownSeconds = document.querySelector("#seconds");
@@ -22,43 +20,57 @@ const resetButton = document.querySelector(".reset");
 const currentRoundElement = document.querySelector("#current-round");
 const totalRoundsElement = document.querySelector("#total-rounds");
 const content = document.querySelector(".content");
+const roundsInput = form.querySelector("#rounds");
+const workInput = form.querySelector("#work");
+const restInput = form.querySelector("#rest");
+const prepInput = form.querySelector("#prep");
 
-// Computation Values
-let rounds = parseInt(form.querySelector("#rounds").value);
-let workSeconds = parseInt(form.querySelector("#work").value);
-let restSeconds = parseInt(form.querySelector("#rest").value);
+// Global Variables
+let timer;
+let prepTimer;
+let elapsedSeconds;
+let totalRoundSeconds;
+let currentRound;
+let isResting;
+let isPaused = false;
+let rounds = parseInt(roundsInput.value);
+let workSeconds = parseInt(workInput.value);
+let restSeconds = parseInt(restInput.value);
+let prepSeconds = parseInt(prepInput.value);
 
-// Event Listener
-form.addEventListener("focusout", () => {
-  render();
-});
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  startTimer();
-});
-
-resetButton.addEventListener("click", () => {
-  resetTimer();
-});
+// Event Listeners
+roundsInput.addEventListener("input", render);
+workInput.addEventListener("input", render);
+restInput.addEventListener("input", render);
+prepInput.addEventListener("input", render);
+form.addEventListener("submit", startTimer);
+resetButton.addEventListener("click", resetTimer);
 
 // Set initial state
 render();
 
-// Functions
-function render() {
-  // Get new values
-  rounds = parseInt(form.querySelector("#rounds").value);
-  workSeconds = parseInt(form.querySelector("#work").value);
-  restSeconds = parseInt(form.querySelector("#rest").value);
+/*
+/* Utility Functions
+*/
+function clearTimers() {
+  clearInterval(timer);
+  clearInterval(prepTimer);
+  timer = null;
+  prepTimer = null;
+}
 
-  // Set new values
-  minutes.innerHTML = `${Math.floor(workSeconds / 60)}`.padStart(2, "0");
-  seconds.innerHTML = `${workSeconds % 60}`.padStart(2, "0");
-  currentRoundElement.innerHTML = 1;
-  totalRoundsElement.innerHTML = rounds;
+function getMinutes(seconds) {
+  return `${Math.floor(seconds / 60)}`.padStart(2, "0");
+}
 
-  // Create progress bars
+function getSeconds(seconds) {
+  return `${seconds % 60}`.padStart(2, "0");
+}
+
+/*
+/* Display Functions
+*/
+function createProgressBars() {
   const totalDuration = rounds * (workSeconds + restSeconds) - restSeconds;
   const progressContainer = document.querySelector("#progress-container");
   progressContainer.innerHTML = "";
@@ -85,37 +97,87 @@ function render() {
   progressOverlay.appendChild(elapsedBar);
 }
 
-function startTimer() {
-  if (timer) {
-    clearInterval(timer);
-  }
+function render() {
+  // Get new values
+  rounds = parseInt(form.querySelector("#rounds").value);
+  workSeconds = parseInt(form.querySelector("#work").value);
+  restSeconds = parseInt(form.querySelector("#rest").value);
 
+  // Set new values
+  minutes.innerHTML = getMinutes(workSeconds);
+  seconds.innerHTML = getSeconds(workSeconds);
+  currentRoundElement.innerHTML = 1;
+  totalRoundsElement.innerHTML = rounds;
+
+  // Create progress bars
+  createProgressBars();
+}
+
+/*
+/* Timer Functions
+*/
+function startTimer(e) {
+  e.preventDefault();
+  console.log("Starting timer");
+
+  clearTimers();
+
+  // Make timer pauseable
+  startButton.innerHTML = "Pause";
+  form.removeEventListener("submit", startTimer);
+  form.addEventListener("submit", pauseTimer);
+
+  // Start preparation countdown
+  if (!isPaused) {
+    prepSeconds = parseInt(prepInput.value);
+    let prepCounter = prepSeconds;
+    countdownMinutes.innerHTML = getMinutes(prepCounter);
+    countdownSeconds.innerHTML = getSeconds(prepCounter);
+    content.classList.add("preparing");
+
+    // Update the preparation countdown every second
+    prepTimer = setInterval(() => {
+      prepCounter--;
+      countdownMinutes.innerHTML = getMinutes(prepCounter);
+      countdownSeconds.innerHTML = getSeconds(prepCounter);
+
+      if (prepCounter === 0) {
+        clearInterval(prepTimer);
+        startWorkout();
+      }
+    }, 1000);
+  } else if (isPaused) {
+    startWorkout();
+  }
+}
+
+function startWorkout() {
   // Update the DOM
   rounds = parseInt(form.querySelector("#rounds").value);
   workSeconds = parseInt(form.querySelector("#work").value);
   restSeconds = parseInt(form.querySelector("#rest").value);
   // audioCue
+  content.classList.remove("preparing");
   content.classList.add("working");
   const elapsedBar = document.querySelector(".progress-bar-elapsed");
 
-  // Change the button from start to pause
-  // startButton.innerHTML = "Pause";
-
   // Set initial values
   isResting = false;
-  currentRound = 1;
-  minutes.innerHTML = "0".padStart(2, "0");
-  seconds.innerHTML = "0".padStart(2, "0");
-  totalRoundsElement.innerHTML = rounds;
-  elapsedSeconds = 1;
-  totalRoundSeconds = workSeconds + restSeconds;
-  totalDuration = rounds * (workSeconds + restSeconds) - restSeconds;
+  if (!isPaused) {
+    currentRound = 1;
+    minutes.innerHTML = getMinutes(0);
+    seconds.innerHTML = getSeconds(0);
+    totalRoundsElement.innerHTML = rounds;
+    elapsedSeconds = 1;
+    totalRoundSeconds = workSeconds + restSeconds;
+    totalDuration = rounds * (workSeconds + restSeconds) - restSeconds;
+  }
 
   // Update the display every 1000 milliseconds
   timer = setInterval(() => {
     // Waits 1 second before running
-    minutes.innerHTML = `${Math.floor(elapsedSeconds / 60)}`.padStart(2, "0");
-    seconds.innerHTML = `${elapsedSeconds % 60}`.padStart(2, "0");
+    minutes.innerHTML = getMinutes(elapsedSeconds);
+    seconds.innerHTML = getSeconds(elapsedSeconds);
 
     secondsLeftInRound =
       totalRoundSeconds - (elapsedSeconds % totalRoundSeconds);
@@ -143,20 +205,40 @@ function startTimer() {
 
     // Update the progress bar
     elapsedBar.style.width = `${(elapsedSeconds / totalDuration) * 100}%`;
-    console.log(`${(elapsedSeconds / totalDuration) * 100}%`);
-    console.log(elapsedBar);
 
     elapsedSeconds++;
   }, 1000);
 }
 
-function resetTimer() {
-  clearInterval(timer);
-  timer = null;
+function pauseTimer(e) {
+  e.preventDefault();
+  console.log("Pausing timer");
+  console.log(`Elapsed seconds: ${elapsedSeconds}`);
+  isPaused = true;
+  clearTimers();
+
+  // Change the button from pause to resume
+  startButton.innerHTML = "Resume";
+  form.removeEventListener("submit", pauseTimer);
+  form.addEventListener("submit", startTimer);
+}
+
+function resetTimer(e) {
+  e.preventDefault();
+
+  // Reset timers
+  clearTimers();
+  isPaused = false;
+  prepSeconds = parseInt(prepInput.value);
   currentRound = 1;
   elapsedSeconds = 0;
+
+  // Reset the DOM
   render();
+  content.classList.remove("preparing");
   content.classList.remove("working");
   content.classList.remove("resting");
   content.classList.remove("finished");
+
+  console.log("Reset timer");
 }
