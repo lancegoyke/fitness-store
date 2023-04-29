f2ccb1e2784e475be88c59c42ff6becbfa2a3c6a
Lance Goyke <lancegoyke@gmail.com>
2023-04-29 12:02:00 -0700

    fix Set up Watchman and django-browser-reload #58 (#61)

--- app/config/urls.py
+++ app/config/urls.py
@@ -44,6 +44,7 @@ if settings.ENVIRONMENT == "DEVELOPMENT":
 
     urlpatterns += [
         path("__debug__/", include(debug_toolbar.urls)),
+        path("__reload__/", include("django_browser_reload.urls")),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.ENVIRONMENT == "PRODUCTION":
