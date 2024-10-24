9218ef404a8cf2c08dc1bdacc4c177875e75dce1
Lance Goyke <lance@lancegoyke.com>
2024-10-23 19:36:15 -0500

    add ssl_require=True to production database config

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -153,8 +153,7 @@ DATABASES = {
 
 # dj_database_url
 if DATABASE_URL := os.environ.get("DATABASE_URL"):
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES["default"].update(db_from_env)
+    DATABASES["default"] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
