02b3bb1d98f11bfb4a306ec804d5426b930edbf7
Lance Goyke <lance@lancegoyke.com>
2025-07-13 10:33:55 -0500

    Provide dummy implementation of factories in case `factory_boy` package is not available

--- app/store_project/products/factories.py
+++ app/store_project/products/factories.py
@@ -1,42 +1,55 @@
-import random
+try:
+    import random
 
-import factory
-from django.utils.text import slugify
-from factory.django import DjangoModelFactory
-from store_project.users.factories import SuperAdminFactory
+    import factory
+    from django.utils.text import slugify
+    from factory.django import DjangoModelFactory
 
-from .models import Book
-from .models import Product
-from .models import Program
+    from store_project.users.factories import SuperAdminFactory
 
+    from .models import Book
+    from .models import Product
+    from .models import Program
 
-class BookFactory(DjangoModelFactory):
-    class Meta:
-        model = Book
+    class BookFactory(DjangoModelFactory):
+        class Meta:
+            model = Book
 
-    name = factory.Faker("sentence", nb_words=5, variable_nb_words=True)
-    description = factory.Faker("paragraph", nb_sentences=2, variable_nb_sentences=True)
-    slug = factory.LazyAttribute(lambda o: slugify(o.name))
-    status = Product.PUBLIC
-    price = 10.00
-    author = factory.SubFactory(SuperAdminFactory)
-    page_content = factory.Faker(
-        "paragraph", nb_sentences=5, variable_nb_sentences=True
-    )
+        name = factory.Faker("sentence", nb_words=5, variable_nb_words=True)
+        description = factory.Faker(
+            "paragraph", nb_sentences=2, variable_nb_sentences=True
+        )
+        slug = factory.LazyAttribute(lambda o: slugify(o.name))
+        status = Product.PUBLIC
+        price = 10.00
+        author = factory.SubFactory(SuperAdminFactory)
+        page_content = factory.Faker(
+            "paragraph", nb_sentences=5, variable_nb_sentences=True
+        )
 
+    class ProgramFactory(DjangoModelFactory):
+        class Meta:
+            model = Program
 
-class ProgramFactory(DjangoModelFactory):
-    class Meta:
-        model = Program
+        name = factory.Faker("sentence", nb_words=5, variable_nb_words=True)
+        description = factory.Faker(
+            "paragraph", nb_sentences=2, variable_nb_sentences=True
+        )
+        slug = factory.LazyAttribute(lambda o: slugify(o.name))
+        status = Product.PUBLIC
+        price = 10.00
+        author = factory.SubFactory(SuperAdminFactory)
+        page_content = factory.Faker(
+            "paragraph", nb_sentences=5, variable_nb_sentences=True
+        )
+        duration = factory.LazyFunction(lambda: random.randint(2, 8))
+        frequency = factory.LazyFunction(lambda: random.randint(3, 7))
 
-    name = factory.Faker("sentence", nb_words=5, variable_nb_words=True)
-    description = factory.Faker("paragraph", nb_sentences=2, variable_nb_sentences=True)
-    slug = factory.LazyAttribute(lambda o: slugify(o.name))
-    status = Product.PUBLIC
-    price = 10.00
-    author = factory.SubFactory(SuperAdminFactory)
-    page_content = factory.Faker(
-        "paragraph", nb_sentences=5, variable_nb_sentences=True
-    )
-    duration = factory.LazyFunction(lambda: random.randint(2, 8))
-    frequency = factory.LazyFunction(lambda: random.randint(3, 7))
+except ImportError:
+    # Factory Boy is not available (likely in production)
+    # Define dummy classes to prevent import errors
+    class BookFactory:
+        pass
+
+    class ProgramFactory:
+        pass
