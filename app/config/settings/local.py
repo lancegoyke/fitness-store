7c237dae1737042a035c95b91964d6df85674836
Lance Goyke <lance@lancegoyke.com>
2025-07-13 10:22:11 -0500

    Move `django-browser-reload` into local settings file

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -6,10 +6,12 @@ from .base import *  # noqa
 
 INSTALLED_APPS += [  # noqa F405
     "debug_toolbar",
+    "django_browser_reload",
 ]
 
 MIDDLEWARE += [  # noqa F405
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 INTERNAL_IPS = [
