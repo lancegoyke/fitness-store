37601279ee3ab2e7f27daf626cfbe64fb80035eb
Lance Goyke <lance@lancegoyke.com>
2025-07-19 09:10:56 -0500

    add new fields to user model

--- app/store_project/users/models.py
+++ app/store_project/users/models.py
@@ -1,8 +1,8 @@
 import uuid
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.db.models import UUIDField
+from django.core.validators import MinValueValidator
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -10,10 +10,28 @@ from django.utils.translation import gettext_lazy as _
 class User(AbstractUser):
     """Default user for store_project."""
 
-    id = UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    stripe_customer_id = CharField(_("Stripe Customer ID"), max_length=100, blank=True)
+    class Sex(models.TextChoices):
+        UNKNOWN = "U", _("Prefer not to say")
+        MALE = "M", _("Male")
+        FEMALE = "F", _("Female")
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    birthday = models.DateField(
+        _("Birthday"),
+        help_text="Please use the following format: YYYY-MM-DD",
+        null=True,
+        blank=True,
+    )
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    points = models.IntegerField(
+        _("Points"), default=0, validators=[MinValueValidator(0)]
+    )
+    sex = models.CharField(
+        _("Sex"), max_length=1, choices=Sex.choices, default=Sex.UNKNOWN
+    )
+    stripe_customer_id = models.CharField(
+        _("Stripe Customer ID"), max_length=100, blank=True
+    )
 
     def __str__(self):
         return self.email
