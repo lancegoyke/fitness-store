579e545f0153ee84fa93ab182ac5ff358d9bf2d2
Lance Goyke <lance@lancegoyke.com>
2025-08-13 08:09:44 -0500

    remove page caching

--- app/store_project/pages/urls.py
+++ app/store_project/pages/urls.py
@@ -1,6 +1,4 @@
-from django.conf import settings
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from store_project.pages.views import HomePageView
 from store_project.pages.views import SinglePageView
@@ -12,19 +10,19 @@ app_name = "pages"
 urlpatterns = [
     path(
         "",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(HomePageView.as_view()),
+        HomePageView.as_view(),
         name="home",
     ),
     path(
         "contact/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(contact_view),
+        contact_view,
         name="contact",
     ),
     path("timer/", timer_view, name="timer"),
     path("robots.txt", robots_txt, name="robots_txt"),
     path(
         "<str:slug>/",
-        cache_page(settings.DEFAULT_CACHE_TIMEOUT)(SinglePageView.as_view()),
+        SinglePageView.as_view(),
         name="single",
     ),
 ]
