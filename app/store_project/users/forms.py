fc2a7f3b8b1ee01b13988e756a0a92f9730941de
Lance Goyke <lance@lancegoyke.com>
2025-08-14 21:02:07 -0500

    Specify unique username when creating a user in the Django admin

--- app/store_project/users/forms.py
+++ app/store_project/users/forms.py
@@ -26,6 +26,14 @@ class UserCreationForm(forms.UserCreationForm):
             "email",
         )
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make username field optional
+        self.fields["username"].required = False
+        self.fields["username"].help_text = _(
+            "Leave blank to auto-generate from email address."
+        )
+
     def clean_email(self):
         email = self.cleaned_data["email"]
 
@@ -37,7 +45,11 @@ class UserCreationForm(forms.UserCreationForm):
         raise ValidationError(self.error_messages["duplicate_email"])
 
     def clean_username(self):
-        username = self.cleaned_data["username"]
+        username = self.cleaned_data.get("username", "").strip()
+
+        # If no username provided, return empty string (will be generated in save())
+        if not username:
+            return ""
 
         try:
             User.objects.get(username=username)
@@ -45,3 +57,23 @@ class UserCreationForm(forms.UserCreationForm):
             return username
 
         raise ValidationError(self.error_messages["duplicate_username"])
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+
+        # If no username was provided, generate one from email
+        if not user.username and user.email:
+            base_username = user.email.split("@")[0]
+            username = base_username
+            counter = 1
+
+            # Ensure username is unique
+            while User.objects.filter(username=username).exists():
+                username = f"{base_username}{counter}"
+                counter += 1
+
+            user.username = username
+
+        if commit:
+            user.save()
+        return user
